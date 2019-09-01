dec182c596d202ebd6d54bee7b46e1933f0d8e31
Dusty Miller <LeoDMiller@gmail.com>
2019-09-01 11:48:32 -0700

    Modified wl_generator.cpp.  Now making the http POST request from within a lambda function that has been scheduled in the thread pool.

--- wl_generator/wl_generator/app/wl_generator.cpp
+++ wl_generator/wl_generator/app/wl_generator.cpp
@@ -21,54 +21,32 @@ int main()
 	std::thread t1(fun);
 	t1.join();
 
+	std::function<void()> fun2 = []() { 
+		std::cout << "running http request" << std::endl; 
+			CURL *curl;
+			CURLcode res;
+			curl = curl_easy_init();
+			if(curl) {
+				struct curl_slist *slist1 = NULL;
+				slist1 = curl_slist_append(slist1, "Content-Type: application/json");
+				curl_easy_setopt(curl, CURLOPT_URL, "http://trans-srv:5555/api/movies");
+				curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+				curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"name\":\"The bigger better movie about workload generators\",\"date\":\"2020\"}");
+				curl_easy_setopt(curl, CURLOPT_HTTPHEADER, slist1);
+				curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "POST");
 
-	CURL *curl;
-	CURLcode res;
-	curl = curl_easy_init();
-	if(curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, "http://transaction-server-quart:5000/something");
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
-		res = curl_easy_perform(curl);
+				// Now actually perform the http request
+				res = curl_easy_perform(curl);
+				res = curl_easy_perform(curl);
 
-		curl_easy_cleanup(curl);
-	}
+				curl_easy_cleanup(curl);
+			}
+	}; // end of lambda function fun2
 
-
-	// The commented out code is an attempt at making use of the SFML library
-	// to make http post requests.
-	/*
-	sf::Http http("http://transaction-server-quart:5000");
-	sf::Http::Request request;
-	request.setMethod(sf::Http::Request::Post);
-	request.setUri("/");
-	request.setHttpVersion(1,1);
-	request.setField("From", "me");
-	request.setField("To", "you");
-	request.setBody("para1=value1&para2=value2");
-
-	sf::Http::Response response = http.sendRequest(request);
-	*/
-	/*
-	std::string name = "random name";
-	int score = 15;
-
-		// prepare the request
-		sf::Http::Request request("/send-score.php", sf::Http::Request::Post);
-
-		// encode the parameters in the request body
-		std::ostringstream stream;
-		stream << "name=" << name << "&score=" << score;
-		request.setBody(stream.str());
-
-		// send the request
-		sf::Http http("http://www.myserver.com/");
-		sf::Http::Response response = http.sendRequest(request);
-
-		 */
 	using function_type = std::function<void()>;
 	const std::size_t QUEUE_SIZE = 500;
 	ra::concurrency::thread_pool tp;
-	tp.schedule(std::move(fun));
+	tp.schedule(std::move(fun2));
 	std::cout << "finished tester, queue and threadpool made" << std::endl;
 	return 0;
 }
