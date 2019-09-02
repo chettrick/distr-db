cda126b31b138ebd81659e965652ea3511f237c7
Chris Hettrick <git@structfoo.com>
2019-09-02 15:42:36 -0700

    Transaction Server: Flesh out all Mongo database writes and reads.
    A GET request to endpoint /api/movies will return, in JSON,
    all records from the Mongo database, (currently) up to 100 records.
    A GET request to endpoint /api/movies/[movie_id] will return, in JSON,
    the record in the Mongo database that has an _id of movie_id.
    
    The serialized id returned from Postgres is used as the special "_id"
    in Mongo, which guarantees that there will not be any duplicate entries.
    
    Many debug print() statements are left in for now.
    
    The call to "cursor = coll.find()" does not need an await, as it only
    connects to the Mongo database when the cursor is used, such as in
    "movie_list = await cursor.to_list()".
    
    Inserting into Mongo will only happen if the insert to Postgres is
    successful, by use of the except catch block, which will skip the
    call to insert_to_mongo().
    
    The sample "movies" dictionary database used for testing is no longer needed.
    
    The get_movies() method that reads from Postgres is not used,
    and is renamed to get_movies_from_postgres(). Keep it for diagnostics.

--- trans-srv/__main__.py
+++ trans-srv/__main__.py
@@ -12,22 +12,6 @@ import motor.motor_asyncio
 app = Quart(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-# Sample database for testing API.
-movies = [
-    {
-        'id': 1,
-        'name': u'Top Gun',
-        'desc': u'One daring young pilot flies jets.',
-        'date': u'1986'
-    },
-    {
-        'id': 2,
-        'name': u'Days of Thunder',
-        'desc': u'One daring young driver drives cars.',
-        'date': u'1990'
-    }
-]
-
 motorclient = None  # Motor (MongoDB) client
 db = None           # MongoDB database
 coll = None         # Movies collection of the database
@@ -53,41 +37,35 @@ def not_found(error):
 async def get_movies():
     # Request all movies directly from the Mongo database.
 
-    # XXX Read from Mongo
-    await do_insert()
+    movie_list = None
 
-    # Mongo database connection
-    print('Connecting to the MongoDB database...')
-    data = {
-        "_id": 59,
-        "name": "The Matrix 59",
-        "date": "2059",
-        "desc": "Neo, in his old age, practices Tai Chi"
-    }
-    print('Inserting data document...')
-    result = await coll.insert_one(data)
-    print('result %s' % repr(result.inserted_id))
-    print('Listing database names...')
-    print(await motorclient.list_database_names())
-    print('Listing inserted id...')
-    print(result.inserted_id)
-    print('Find document by _id and print it out...')
-    print(await coll.find_one({'_id': 59}))
-    print('Database connection closed.')
+    # Read from Mongo into a Python list.
+    cursor = coll.find()
+    movie_list = await cursor.to_list(length=100)
+    print("GET request to return all movies")
+    print("returned movie list: ", movie_list)
 
-    return jsonify({'movies': movies})
+    if movie_list is None:
+        abort(404)
+
+    return jsonify({'movies': movie_list})
 
 
 @app.route('/api/movies/<int:movie_id>', methods = ['GET'])
 async def get_movie(movie_id):
     # Request a movie with id of movie_id from the Mongo database.
 
-    # XXX Read from Mongo
-    movie = [movie for movie in movies if movie['id'] == movie_id]
-    if len(movie) == 0:
+    the_movie = None
+
+    # Read from Mongo via movie_id.
+    the_movie = await coll.find_one({'_id': movie_id})
+    print("GET request to return one movie")
+    print("returned movie: ", the_movie)
+
+    if the_movie is None:
         abort(404)
 
-    return jsonify({'movie': movie[0]})
+    return jsonify({'movie': the_movie})
 
 
 @app.route('/api/movies', methods = ['POST'])
@@ -152,6 +130,11 @@ async def insert_movie(movie_name, movie_date, movie_desc):
         await conn.close()
 
         print("Movie written to db, no error occured")
+
+        # The INSERT to Postgres was successful,
+        # so now write the record to the Mongo database.
+        await insert_to_mongo(movie_id, movie_name, movie_date, movie_desc)
+
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error occured:", error)
     finally:
@@ -162,7 +145,7 @@ async def insert_movie(movie_name, movie_date, movie_desc):
     return movie_id
 
 
-def get_movies():
+def get_movies_from_postgres():
     """ query data from the movies table """
     conn = None
     try:
@@ -185,25 +168,25 @@ def get_movies():
             conn.close()
 
 
-async def do_insert():
+async def insert_to_mongo(movie_id, movie_name, movie_date, movie_desc):
     # Mongo database connection
     print('Connecting to the MongoDB database...')
-    data = {
-        "_id": 42,
-        "name": "The Hitchhikers Get Lost",
-        "date": "20042",
-        "desc": "The Meaning of Life is Survival"
+    new_movie = {
+        '_id': movie_id,
+        'name': movie_name,
+        'date': movie_date,
+        'desc': movie_desc
     }
     print('Inserting data record...')
-    result = await coll.insert_one(data)
+    result = await coll.insert_one(new_movie)
     print('result %s' % repr(result.inserted_id))
-    print('Listing database names...')
-    print(await motorclient.list_database_names())
     print('Listing inserted id...')
     print(result.inserted_id)
     print('Print out first document...')
     print(await coll.find_one())
     print('Database connection closed.')
+
+    return result
 
 
 if __name__ == '__main__':
